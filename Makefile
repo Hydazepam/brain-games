8a4540c2a4b1045eed81d5160e2569fadd939911
Akadem <kostianetskyi@gmail.com>
2019-06-04 10:35:02 +0300

    update

--- Makefile
+++ Makefile
@@ -5,6 +5,7 @@ install:
 
 start:
 	npx babel-node src/bin/brain-games.js
+	npx babel-node src/bin/brain-even.js
 
 publish:
 	publish --dry-run
--- src/index.js
+++ src/index.js
@@ -6,3 +6,32 @@ const getName = () => {
 };
 
 export default getName;
+
+export const brainEven = () => {
+  console.log('Welcome to the Brain Games!\nAnswer "yes" if number even otherwise answer "no".\n');
+
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName} !\n`);
+
+  const isEven = () => {
+    const gameRounds = 3;
+    const randomNum = (min, max) => Math.floor(Math.random() * (max - min)) + min;
+    for (let i = 1; i <= gameRounds; i += 1) {
+      const currentNum = randomNum(1, 101);
+      console.log(`Question: ${currentNum}`);
+      const curAnswer = readlineSync.question('Your answer: ');
+      let result;
+      if ((currentNum % 2 === 0 && curAnswer === 'yes') || (currentNum % 2 !== 0 && curAnswer === 'no')) {
+        result = 'Correct';
+      } else {
+        result = `'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${userName}!`;
+        console.log(result);
+        return result;
+      }
+      console.log(result);
+    }
+    console.log(`Congratulations, ${userName}!`);
+  };
+
+  isEven();
+};

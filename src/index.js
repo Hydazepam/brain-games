4920f1ba8f86b8a5d3ddc1b7e85e8a04ec65e46c
Akadem <kostianetskyi@gmail.com>
2019-06-10 13:13:43 +0300

    update

--- src/index.js
+++ src/index.js
@@ -102,3 +102,59 @@ export const brainCalc = () => {
 
   calc();
 };
+
+export const brainGcd = () => {
+  console.log('Welcome to the Brain Games!\nFind the greatest common divisor of given numbers.\n');
+
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName} !\n`);
+
+  const gcd = () => {
+    const gameRounds = 3;
+    const randomNum = (min, max) => Math.floor(Math.random() * (max - min)) + min;
+
+    for (let i = 1; i <= gameRounds; i += 1) {
+      const firstNum = randomNum(1, 101);
+      const secondNum = randomNum(1, 101);
+
+      const calcQuestion = () => `${firstNum} ${secondNum}`;
+
+      console.log(`Question: ${calcQuestion()}`);
+      const curAnswer = readlineSync.question('Your answer: ');
+
+      const curResult = () => {
+        let result;
+        if (firstNum > secondNum) {
+          for (let x = secondNum; x > 0; x -= 1) {
+            if (firstNum % x === 0 && secondNum % x === 0) {
+              result = x;
+              return result;
+            }
+          }
+        } else if (firstNum < secondNum) {
+          for (let x = firstNum; x > 0; x -= 1) {
+            if (firstNum % x === 0 && secondNum % x === 0) {
+              result = x;
+              return result;
+            }
+          }
+        }
+        return firstNum;
+      };
+
+      let result;
+      if (Number(curAnswer) === curResult()) {
+        result = 'Correct';
+      } else {
+        result = `'${curAnswer}' is wrong answer ;(. Correct answer was '${curResult()}'.\nLet's try again, ${userName}!`;
+        console.log(result);
+        return result;
+      }
+      console.log(result);
+    }
+    console.log(`Congratulations, ${userName}!`);
+    return '';
+  };
+
+  gcd();
+};

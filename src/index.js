9aac00f2067380afc0349ce661a84ed84cbfa87e
Akadem <kostianetskyi@gmail.com>
2019-06-26 11:15:57 +0300

    update

--- src/index.js
+++ src/index.js
@@ -9,39 +9,18 @@ const gamePlay = (task, currentData, currentQuestion, currentAnswer) => {
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName} !\n`);
 
-  let resultOfGame;
-  let points = 0;
-
   for (let r = 1; r <= gameRounds; r += 1) {
     const currentRound = currentData();
     console.log(`Question: ${currentQuestion(currentRound)}`);
     const gamerAnswer = readlineSync.question('Your answer: ');
     const correctAnswer = currentAnswer(currentRound);
 
-    let result;
     if (gamerAnswer === correctAnswer) {
-      result = 'Correct';
-    } else {
-      result = `'${gamerAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`;
-      console.log(result);
-      break;
+      console.log('Correct');
     }
-    console.log(result);
-
-    if (result === 'Correct') {
-      points += 1;
-    } else {
-      points = 0;
-    }
+    return `'${gamerAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${userName}!`;
   }
-
-  if (points === gameRounds) {
-    resultOfGame = `Congratulations, ${userName}!`;
-  } else {
-    resultOfGame = `Let's try again, ${userName}!`;
-  }
-  console.log(resultOfGame);
-  return resultOfGame;
+  return `Congratulations, ${userName}!`;
 };
 
 export { gamePlay, randomNum };

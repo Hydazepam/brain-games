d2ae1e01a52c3a035266934083ed8da4d1e9a7d3
Akadem <kostianetskyi@gmail.com>
2019-06-27 15:37:01 +0300

    update

--- src/index.js
+++ src/index.js
@@ -9,39 +9,21 @@ const playGame = (task, makeGame) => {
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName} !\n`);
 
-  let resultOfGame;
-  let points = 0;
-
   for (let i = 1; i <= roundsCount; i += 1) {
     const roundData = makeGame();
     console.log(`Question: ${car(roundData)}`);
     const gamerAnswer = readlineSync.question('Your answer: ');
     const correctAnswer = cdr(roundData);
 
-    let result;
     if (gamerAnswer === correctAnswer) {
-      result = 'Correct';
+      console.log('Correct');
     } else {
-      result = `'${gamerAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`;
-      console.log(result);
-      break;
-    }
-    console.log(result);
-
-    if (result === 'Correct') {
-      points += 1;
-    } else {
-      points = 0;
+      console.log(`'${gamerAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      console.log(`Let's try again, ${userName}!`);
+      return;
     }
   }
-
-  if (points === roundsCount) {
-    resultOfGame = `Congratulations, ${userName}!`;
-  } else {
-    resultOfGame = `Let's try again, ${userName}!`;
-  }
-  console.log(resultOfGame);
-  return resultOfGame;
+  console.log(`Congratulations, ${userName}!`);
 };
 
 export default playGame;
